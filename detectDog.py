70d225ad95d5861b19c64d57a284f02ddc165340
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2022-02-20 14:46:28 +0530

    Implement dog detection function and django backend

---
+++ detectDog.py
@@ -0,0 +1,41 @@
+# pip install opencv-python
+# pip install keras
+# pip install tqdm
+import cv2
+
+# Load pre-trained classifier for dog detection (xml file)
+# face_cascade = cv2.CascadeClassifier('')
+
+# All imports
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from tqdm import tqdm
+import numpy as np
+
+ResNet50_model = ResNet50(weights='imagenet')
+
+def path_to_tensor(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    return np.expand_dims(x, axis=0)
+
+
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+
+def ResNet50_predict_labels(img_path):
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return prediction <= 268 & prediction >= 151
+
+print(dog_detector('dog1.jpg'))
